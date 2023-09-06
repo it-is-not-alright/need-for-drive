32a09a2c4299d45b4fa9815b65c58634ac140ad1
it-is-not-alright <maksimminarov045@gmail.com>
2023-09-07 02:03:11 +0400

    deploy

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import { React, StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './App';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  // eslint-disable-next-line react/jsx-filename-extension
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);

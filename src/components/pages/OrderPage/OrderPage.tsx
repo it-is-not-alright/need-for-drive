16bf20f1e29c901d9257b8449236523e6b92b3ae
it-is-not-alright <maksimminarov045@gmail.com>
2023-09-24 17:56:42 +0400

    V-2. OrderInfo component

--- src/components/pages/OrderPage/OrderPage.tsx
+++ src/components/pages/OrderPage/OrderPage.tsx
@@ -5,6 +5,7 @@ import React, { useState } from 'react';
 import { OrderPageProps } from './types';
 import Breadcrumbs from '../../Breadcrumbs/Breadcrumbs';
 import LocationStage from './stages/LocationStage/LocationStage';
+import OrderInfo from './OrderInfo/OrderInfo';
 
 function OrderPage({ header }: OrderPageProps) {
   const [city, setCity] = useState<string>('');
@@ -27,25 +28,7 @@ function OrderPage({ header }: OrderPageProps) {
           setPoint={setPoint}
         />
         <div className="line-vertical" />
-        <div id="order-page__content__check">
-          <p className="dark-text fw-500 ta-right fs-3">Ваш заказ:</p>
-          <div id="order-page__content__check__point" className="fw-300">
-            <p className="dark-text">Пункт выдачи</p>
-            <div className="line-dotted" />
-            <p className="gray-text">
-              <span>Ульяновск,</span>
-              <br />
-              <span>Нариманова 42</span>
-            </p>
-          </div>
-          <p className="dark-text fs-2">
-            <span className="fw-500">Цена: </span>
-            <span>от 8 000 до 12 000 ₽</span>
-          </p>
-          <button className="btn-large" type="button">
-            Выбрать модель
-          </button>
-        </div>
+        <OrderInfo />
       </div>
     </div>
   );

01566dc74e8bf366d651085f49891d7083d2edc5
it-is-not-alright <maksimminarov045@gmail.com>
2023-10-05 12:47:18 +0400

    F-2. Breadcrumbs fix

--- src/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -1,6 +1,7 @@
 import './style.scss';
 
-import React, { Fragment, ReactElement } from 'react';
+import classNames from 'classnames';
+import React, { Fragment } from 'react';
 
 import Icon from '../Icon/Icon';
 import BreadcrumbsProps from './types';
@@ -11,26 +12,19 @@ function Breadcrumbs({
   availableIndex,
   onIndexChange,
 }: BreadcrumbsProps) {
-  function renderItem(item: string, index: number): ReactElement {
-    if (index === activeIndex) {
-      return <p className="green-text fw-700">{item}</p>;
-    }
-    if (index <= availableIndex) {
-      return (
-        <button type="button" onClick={() => onIndexChange(index)}>
-          {item}
-        </button>
-      );
-    }
-    return <p className="gray-text fw-700">{item}</p>;
-  }
-
   return (
     <div className="breadcrumbs">
       {items.map((item, index) => {
         return (
           <Fragment key={item}>
-            {renderItem(item, index)}
+            <button
+              type="button"
+              onClick={() => onIndexChange(index)}
+              className={classNames({ active: index === activeIndex })}
+              disabled={index > availableIndex}
+            >
+              {item}
+            </button>
             {index !== items.length - 1 && (
               <Icon name="breadcrumbs-arrow" width={6} height={8} />
             )}

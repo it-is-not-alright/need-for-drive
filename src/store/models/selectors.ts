99f2af1425ed2e083e760aca90e93fd32d758fb9
it-is-not-alright <maksimminarov045@gmail.com>
2023-11-09 17:00:10 +0400

    F-4. Reselect fix

--- src/store/models/selectors.ts
+++ src/store/models/selectors.ts
@@ -1,4 +1,4 @@
-import { createSelector } from 'reselect';
+import { createSelector } from '@reduxjs/toolkit';
 
 import { RootState } from '../root';
 import { IModel, RequestState } from '../types';

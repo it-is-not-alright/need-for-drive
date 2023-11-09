57bb18d68dd21b186419cffce417add09f39aaa4
it-is-not-alright <maksimminarov045@gmail.com>
2023-11-10 01:34:26 +0400

    Revert "Revert "Revert "Revert "Merge pull request #8 from it-is-not-alright/V-4""""
    This reverts commit a5a95f12101a724b6651c3a4197f3ee415364b32.

---
+++ src/components/DateTimePicker/constants.ts
@@ -0,0 +1,17 @@
+export const weekdays = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
+export const spinnerHours: number[] = Array.from(Array(24).keys());
+export const spinnerMinutes: number[] = Array.from(Array(60).keys());
+export const months = [
+  'Январь',
+  'Февраль',
+  'Март',
+  'Апрель',
+  'Май',
+  'Июнь',
+  'Июль',
+  'Август',
+  'Сентябрь',
+  'Октябрь',
+  'Ноябрь',
+  'Декабрь',
+];
